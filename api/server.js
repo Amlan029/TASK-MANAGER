2733b5ffbd46c6e57b3ce71c477b38af9ce86f44
Amlan Mukherjee <101513169+Amlan029@users.noreply.github.com>
2023-04-06 22:15:02 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -9,7 +9,7 @@ app.use(cors());
 
 mongoose
   .connect(
-    "mongodb+srv://amlanc:SIMdUx9NaiVJK1zQ@cluster0.6u037jj.mongodb.net/?retryWrites=true&w=majority",
+    "mongodb+srv://amlanc:SIMdUx9NaiVJK1zQ@cluster0.6u037jj.mongodb.net/test?retryWrites=true&w=majority",
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
